8f1cf0f2d323cfcdf15b104a487721bb05e96eff
Daxaxa13 <pdl13@yandex.ru>
2022-11-29 01:02:47 +0300

    Добавлена проверка паролей из чёрного списка

--- Bruteforce/form_fix.php
+++ Bruteforce/form_fix.php
@@ -1,18 +1,18 @@
 <?php
-if( isset($_POST['Login']) && isset($_POST['username']) && isset($_POST['username']) ) {
+if( isset($_POST['Login']) && isset($_POST['username']) && isset($_POST['password']) ) {
 	
-    $user = stripslashes($_POST['username']);
+    $user = $_POST['username'];
 
     $total_failed_login = 3;  
     $lockout_time       = 5;  
     $account_locked     = false; 
 
-    $data = $db->prepare( 'SELECT failed_login, last_login FROM users WHERE user = (:user) LIMIT 1;' );  
+    $data = $db->prepare( 'SELECT id, failed_login, last_login, email, unlock_token FROM users WHERE user = (:user) LIMIT 1;' );  
     $data->bindParam( ':user', $user, PDO::PARAM_STR );  
     $data->execute();  
-    $row = $data->fetch();  
+    $row = $data->fetch();	
  
-    if (($data->rowCount() == 1) && ($row['failed_login'] >= $total_failed_login)) {  
+    if (($data->rowCount() == 1) && ($row['failed_login'] >= $total_failed_login) && ($row['unlock_token'] == NULL)) {  
 	
         $last_login = strtotime( $row[ 'last_login' ] );  
         $timeout    = $last_login + ($lockout_time * 60);  
@@ -22,10 +22,23 @@ if( isset($_POST['Login']) && isset($_POST['username']) && isset($_POST['usernam
             $account_locked = true;  
         }  
     }  
+	
+	$passwd = $_POST['password']
+	$black_list_passwd = file('passwords.txt');
+	foreach ($black_list_passwd as $line_num => $black_passwd) {
+		if ($black_passwd == $passwd)
+			$email = $row['email']
+			$uniqid_token = uniqid();
+			$url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			$url = explode('?', $url);
+			$url = $url[0];
+			$url = $url . '?' . (string)$uniqid_token
+			$data = $db->prepare( 'UPDATE users SET unlock_token = '$uniqid_token' WHERE id = '$row['id']'');  
+			$message = 'Ваша учётная запись была скомпрометирована, для защиты аккаунта он был заблокирован, перейдите по ссылке {$url} для воостановления доступа'
+			mail($email, 'Восстановление доступа', $message);
+	}
 
-    $pass = md5( stripslashes( $_POST['password'] ) );  
-
-    // MD5 -> SHA256 (pass + get_salt($user));
+    $pass = sha256( $passwd, get_salt($user));  
 
     $data = $db->prepare( 'SELECT * FROM users WHERE user = (:user) AND password = (:password) LIMIT 1;' );  
     $data->bindParam( ':user', $user, PDO::PARAM_STR);  
@@ -63,5 +76,14 @@ if( isset($_POST['Login']) && isset($_POST['username']) && isset($_POST['usernam
     $data->bindParam( ':user', $user, PDO::PARAM_STR );  
     $data->execute();
 }
- 
+else {
+	$url = $_SERVER['QUERY_STRING'];
+	$url = explode('=', $url);
+	$unlock_token = $url[1]
+	
+	$data = $db->prepare( 'SELECT id FROM users WHERE unlock_token = '$unlock_token';' );   
+    $data->execute();  
+    $row = $data->fetch();
+	$data = $db->prepare( 'UPDATE users SET unlock_token = NULL WHERE id = '$row['id']'');  
+}
 ?>
